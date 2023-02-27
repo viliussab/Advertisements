da2b072853d5fea788c11d6d5317420314dd5603
viliussab <viliussab@github.com>
2023-02-27 17:23:09 +0200

    first commit

---
+++ Advertisements.Frontend/src/config/route-paths.ts
@@ -0,0 +1,27 @@
+const route_paths = {
+    objects: {
+        map: "/objects/map",
+        edit: "/objects/{:id}",
+        create: "/objects/create",
+        main: "/objects",
+    },
+    campaigns: {
+        create: "/campaigns/create",
+        edit: "/campaigns/{:id}",
+        main: "/",
+        weekly_overview: "/campaigns/weekly-overview"
+    },
+    customers: {
+        create: "/customers/create",
+        edit: "/customers/{:id}",
+        main: "/customers",
+    },
+    adverts: {
+        weekly_occupancy: "/adverts/occupancy",
+    },
+    auth: {
+        login: "/login",
+    }
+}
+
+export default route_paths;
